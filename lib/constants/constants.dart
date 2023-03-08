11b1c6709c96018bed9a6c3bb4309034159b58c9
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-03-08 18:57:36 +0200

    add chatWidget

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/text_widget.dart';
+
 Color scaffoldBackgroundColor = const Color(0xFF343541);
 Color cardColor = const Color(0xFF444654);
 
